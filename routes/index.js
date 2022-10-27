6d0321f7b2d055725732ee44021ebe11f327b1a0
ncoolidgotu <nathanael.coolidge@ontariotechu.net>
2022-10-26 23:28:07 -0400

    modified:   public/Content/app.css 	modified:   routes/index.js 	new file:   views/about.ejs 	new file:   views/contact.ejs 	modified:   views/index.ejs 	modified:   views/partials/footer.ejs 	modified:   views/partials/main_nav.ejs 	new file:   views/projects.ejs

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+//CRUD --> Create, Read, Update, Delete
+// Post - Get - Put - Delete
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) { //Router is like a mini-app - the better approach than app.get
   res.render('index', { title: 'Home'});
 });
 
@@ -13,17 +17,17 @@ router.get('/home', function(req, res, next) {
 
 /* GET projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'Projects' });
+  res.render('projects', { title: 'Projects' });
 });
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About Me' });
+  res.render('about', { title: 'About' });
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact Me' });
+  res.render('index', { title: 'Contact' });
 });
 
-module.exports = router;
+module.exports = router; //Make it public
