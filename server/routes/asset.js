2846007de60124248ac388da5e1b9758e8306e19
ncoolidgotu <nathanael.coolidge@ontariotechu.net>
2022-11-16 14:06:12 -0500

    modified:   .DS_Store 	modified:   server/routes/asset.js 	deleted:    server/views/asset.ejs 	modified:   server/views/asset/add.ejs 	modified:   server/views/asset/list.ejs 	deleted:    server/views/contact.ejs 	modified:   server/views/partials/header.ejs 	modified:   server/views/partials/main_nav.ejs

--- server/routes/asset.js
+++ server/routes/asset.js
@@ -18,7 +18,7 @@ router.get('/',(req, res, next)=>{
         }
         else
         {
-            res.render('asset',{
+            res.render('asset/list',{
                 title: 'Asset List', 
                 Assetlist: assetlist,
             })
@@ -31,13 +31,32 @@ router.get('/',(req, res, next)=>{
 
 // Get route for displaying the Add page
 router.get('/add',(req, res, next)=>{
-
+    res.render('asset/add',{title: 'Add Asset'})
 });
 
 // Post route for processing the Add page
 
 router.post('/add',(req, res, next)=>{
-
+    let newAsset = Asset ({
+        "name":req.body.name,
+        "class":req.body.class,
+        "tag":req.body.tag,
+        "description":req.body.description,
+        "aquisitionDate":req.body.aquisitionDate,
+        "usefulLife":req.body.usefulLife,
+        "cost":req.body.cost,
+    });
+    Asset.create(newAsset,(err,Asset) => { //add the asset to the database based on above information specified
+        if(err)
+        {
+            console.log(err)
+            res.end(err)
+        }
+        else //redirect to the list page now that we have added the asset into the database
+        {
+            res.redirect('/asset-list')
+        }
+    })
 });
 
 
@@ -45,21 +64,62 @@ router.post('/add',(req, res, next)=>{
 
 // Get route for displaying the Edit page
 router.get('/edit/:id',(req, res, next)=>{
-
+    let id = req.params.id;
+    Asset.findById(id,(err,assetToEdit)=>{
+        if(err)
+        {
+            console.log(err)
+            res.end(err)
+        }
+        else
+        {
+            res.render('asset/edit',{title:'Update Asset', asset:assetToEdit})
+        }
+    })
 });
 
 // Post route for processing the Edit page
 
 router.post('/edit/:id',(req, res, next)=>{
-
+    let id = req.params.id;
+    let updateAsset = Asset({
+        "_id":id,
+        "name":req.body.name,
+        "class":req.body.class,
+        "tag":req.body.tag,
+        "description":req.body.description,
+        "aquisitionDate":req.body.aquisitionDate,
+        "usefulLife":req.body.usefulLife,
+        "cost":req.body.cost
+    });
+    Asset.updateOne({_id:id}, updateAsset,(err) => {
+        if(err)
+        {
+            console.log(err)
+            res.end(err)
+        }
+        else //redirect to the list page now that we have updated the asset the database
+        {
+            res.redirect('/asset-list')
+        }
+    })
 });
 
 // Delete operation
 
 // Get to perform delete operation
-router.get('/delete/:id',(req, res, next)=>{
-
+router.get('/delete/:id',(req, res, next)=> {
+    let id = req.params.id;
+    Asset.remove({_id:id}, (err) =>{
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else //redirect to the list page now that we have deleted the asset from the database
+        {
+            res.redirect('/asset-list');
+        }
+    })
 });
-
-
 module.exports=router;
